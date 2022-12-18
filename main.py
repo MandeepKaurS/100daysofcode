064f01d7466b5d5400b669651547b05c60496f57
MandeepKaurS <118074546+MandeepKaurS@users.noreply.github.com>
2022-12-18 08:58:06 -0500

    Motivational Quote Email Alert!

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
-# Write your code below this line 👇
+import datetime as dt
+import smtplib
+import random
+def fetchQuote():
+    quotes_file=open("./quotes.txt","r", encoding='utf-8')
+    list_of_quotes = quotes_file.readlines()
 
+    number_of_quotes = len(list_of_quotes)
+    my_quote=list_of_quotes[random.randrange(number_of_quotes)]
+    return my_quote
+today=dt.datetime.now().weekday()
+print(today)
+q = str(fetchQuote())
+if(today==6):
+    with smtplib.SMTP("smtp.gmail.com",587) as connection:
+        connection.ehlo()
+        connection.starttls()
+        connection.login("MandeeepShergill94@gmail.com",password="ftcyowflodtsewai")
+        connection.sendmail(from_addr="MandeeepShergill94@gmail.com", to_addrs="MaanoShergill@gmail.com",msg="SUbject - Motivation Dose!\n\n"+str(q.encode('utf-8')))
 
-def add(*args):
-    l = len(args)
-    sum = 0
-    i = 0
-    while (i < l):
-        sum += args[i]
-        i = i + 1
-    return sum
-
-
-print("total:", add(2, 3, 4, 5))
