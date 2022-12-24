901d58b87278106fbe2420bafcf34c3693f05787
MandeepKaurS <118074546+MandeepKaurS@users.noreply.github.com>
2022-12-24 01:02:19 -0500

    Create LifeInWeeks.py
    Using f-string

---
+++ LifeInWeeks.py
@@ -0,0 +1,14 @@
+# 🚨 Don't change the code below 👇
+age = input("What is your current age? ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+
+
+
+
+year_left=90-int(age)
+months = year_left * 12
+weeks= year_left * 52
+days = year_left * 365
+print(f"You have {days} days, {weeks} weeks, and {months} months left.")
