9f15560bdf83887056cd79e0939c9ad8196b2e94
MandeepKaurS <118074546+MandeepKaurS@users.noreply.github.com>
2023-01-01 19:36:15 -0500

    Create TreasureLand.py

---
+++ TreasureLand.py
@@ -0,0 +1,46 @@
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.") 
+
+#https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
+
+#Write your code below this line 👇
+
+move=input('Where do you want to go? left or right?')
+if(move == "left"):
+    next_move=int(input("You have reached a lake. Do you want to swim or wait for the boat? Swim - type 1, wait type 2"))
+    if(next_move==2):
+        last_move=int(input("Finally, you got onto the other side of the lake.Now you have to choose one of three doors. Type 1 for Red, 2 for Yellow and 3 for Blue door."))
+        if(last_move==1):
+            print('Burned by fire. Game over!')
+        elif(last_move==2):
+            print('You Win!!!')
+        elif(last_move==3):
+            print('Eaten by beasts. Game Over.')
+        else:
+            print('Game Over!')
+    else:
+        print('Attacked by trout. Game Over!')
+else:
+    print("Fell into a hole. Game Over!")
